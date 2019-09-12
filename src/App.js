6da54856ebb7d6f193be57956a73a63b7ea33fec
Parshva Shah <parshvashah@Parshvas-MBP.hitronhub.home>
2019-09-12 00:33:48 -0400

    First deployment

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Typing from 'react-typing-animation';
+import Title from './Components/title';
+
+function App() {
+  return <Title></Title>;
+}
+
+export default App;
---
+++ src/Components/title.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Typing from 'react-typing-animation';
+
+class Title extends React.Component {
+  render() {
+    return <Typing>Deal Grabber</Typing>;
+  }
+}
+
+export default Title;
